11a49d98423836a71d7bc9f4811818e1f25daa6a
fangyou2 <fangyouvip@163.com>
2016-12-30 14:39:17 +0800

    第三模块业务2 廖泽松

--- src/main/java/com/zigong/dao/LogDao.java
+++ src/main/java/com/zigong/dao/LogDao.java
@@ -7,11 +7,13 @@ import com.zigong.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 /**
  * Created by Administrator on 2016/12/27.
  */
 @Component
 public interface LogDao extends JpaRepository<Log, Integer> {
     public Log getLogByLogId(int id);
-    public Log getLogByUser(User user);
+    public Set<Log> getLogByUser(User user);
 }
