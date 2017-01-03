3f650e9bb5e8450428f0f5b3ab6251245e2b0883
rojian <1184571251@qq.com>
2017-01-03 16:01:46 +0800

    Project service

--- src/main/java/com/zigong/service/ProjectService.java
+++ src/main/java/com/zigong/service/ProjectService.java
@@ -5,6 +5,8 @@ import com.zigong.model.project.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * Created by 罗健 on 2016/12/29.
  */
@@ -18,6 +20,34 @@ public class ProjectService {
      */
     public void addProject(Project project) {
         projectDao.save(project);
+    }
 
+    /**
+     * 查询单一项目
+     */
+    public Project findProject(Integer projectNumber) {
+        return  projectDao.findByProjectNumber(projectNumber);
+
+    }
+
+    /**
+     * 查询所有项目
+     */
+    public List<Project> findAllProject() {
+        return projectDao.findAll();
+    }
+
+    /**
+     * 修改项目
+     */
+    public void updateProject(Project project) {
+        projectDao.save(project);
+    }
+
+    /**
+     * 删除项目
+     */
+    public void deleteProject(Integer projectNumber) {
+        projectDao.delete(projectNumber);
     }
 }
